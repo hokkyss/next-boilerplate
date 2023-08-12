352159a8dd19b21998a930e89ef3076855555a18
Hokki Suwanda <hokkyss2@gmail.com>
2023-08-12 20:01:43 +0700

    feat: svgr for inline svg

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import 'server-only';
 
 import Image from 'next/image';
 
+import NextLogo from './_libs/_common/_assets/next.svg';
 import TodoService from './_libs/_common/_services/todo.service';
 
 export default async function Home() {
@@ -27,14 +28,7 @@ export default async function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          alt="Next.js Logo"
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          height={37}
-          priority
-          src="/next.svg"
-          width={180}
-        />
+        <NextLogo className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" height={37} width={180} />
       </div>
       {todos.map((todo) => (
         <p key={todo.id}>{todo.title}</p>
